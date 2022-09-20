5e2911cabbfa53c9182995331eaa4a2001a4d0dc
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-09-20 10:09:30 -0300

    Fazendo Responsividade Index

--- desenvolvendoAtiv/cods/inicio/index.php
+++ desenvolvendoAtiv/cods/inicio/index.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<link rel="stylesheet" type="text/css" href="../../css/style-inicio.css">
 	<link rel="stylesheet" type="text/css" href="../../css/style-md.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style-sm.css">
 
 </head>
 <body>
