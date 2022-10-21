a1c2277de96b4ac9d86fbcea52b181625f331451
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-10-20 22:43:09 -0300

    Delete desenvolvendoAtiv directory

--- desenvolvendoAtiv/cods/user/api/pegaExercicio.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require "../../conexao/conexao.php"; 
-	$id = $_GET['id'];
-	$sql = "SELECT * FROM `videosDesafio` WHERE `id_desafio` = '$id'";
-	$consulta = mysqli_query($cn, $sql);
-	$exibe = mysqli_fetch_all($consulta, MYSQLI_ASSOC);
-	echo json_encode($exibe);
- ?>
