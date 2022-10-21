8ff4f3d1c6893ebdb5ce7f2d81ef58e54f5cae6d
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-10-20 22:43:32 -0300

    Delete js directory

--- js/js-desafiododia.js
+++
@@ -1,1 +0,0 @@
-const idExercicio = document.querySelectorAll(".dias-treinodehoje ul #")
--- js/js-menu.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector(".icon-menu")
-menu.addEventListener("click", () => {
-	const header = document.querySelector(".menu-usua")
-	header.classList.toggle("active")
-})
