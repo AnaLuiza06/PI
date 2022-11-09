1dbd141809000bfa6cd1a1208282eef29e1697e2
BhryanS2 <bhryanstepenhen@gmail.com>
2022-11-08 21:12:05 -0300

    add server db

--- desenvolvendoAtiv/cods/conexao/conexao.php
+++ desenvolvendoAtiv/cods/conexao/conexao.php
@@ -1,10 +1,16 @@
 <?php
-	$servidor = 'localhost';
-	$usuario = 'root';
-	$senha = '';
-	$banco = 'bd_ativ';
+// get server name from URL
+$server = $_SERVER['SERVER_NAME'];
+if ($server == 'localhost') {
+  $servidor = 'localhost';
+  $usuario = 'root';
+  $senha = '';
+  $banco = 'bd_ativ';
+} else {
+  $servidor = 'sql113.epizy.com';
+  $usuario = 'epiz_32816813';
+  $senha = 'DE6dUvssUGIdRiE';
+  $banco = 'epiz_32816813_bd_ativ';
+}
 
-	$cn = mysqli_connect($servidor, $usuario, $senha, $banco);
-
-?>
-
+$cn = mysqli_connect($servidor, $usuario, $senha, $banco);
